dd545e683283f9f971c6721d31f1bd1bc5e2fa52
gulia12345 <134226363+gulia12345@users.noreply.github.com>
2023-05-25 12:53:55 +0100

    yr

---
+++ Scripts/main.js
@@ -0,0 +1,12 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','images/firefox-icon1.png');
+  } else {
+    myImage.setAttribute ('src',"images/firefox-icon.png");
+  }
+}
